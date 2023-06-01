c2a13cf493e96985756e45f2a576428fe2fe1256
st0H034 <kd1307152@st.kobedenshi.ac.jp>
2023-06-01 19:55:45 +0900

    add 初期コミット

---
+++ resources/js/Pages/InertiaTest.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { Link } from '@inertiajs/vue3'
+</script>
+
+<template>
+    inertiaテストです。<br>
+    <a href="/">aタグ経由です</a><br>
+    <Link href="/">Link経由です</Link>
+
+</template>
---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    // WSL2でvite開発サーバへアクセスするために必須
+    server: {
+        hmr: {
+            host: 'localhost',
+        },
+    },
+    plugins: [
+        laravel({
+            input: 'resources/js/app.js',
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+            // WSL2でvite開発サーバへアクセスするために必須
+            serve: {
+                host: true,
+                hmr: {
+                    host: 'localhost',
+                },
+            },
+        }),
+    ],
+});
