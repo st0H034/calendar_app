f1c912a17649ebe8105ad41311dba240c7d61685
st0H034 <kd1307152@st.kobedenshi.ac.jp>
2023-06-07 11:05:17 +0900

    add ルートパラメータの練習

--- resources/js/Pages/InertiaTest.vue
+++ resources/js/Pages/InertiaTest.vue
@@ -1,12 +1,14 @@
 <script setup>
 import { Link } from '@inertiajs/vue3'
+
 </script>
 
 <template>
     inertiaテストです。<br>
     <a href="/">aタグ経由です</a><br>
     <Link href="/">Link経由です</Link><br>
-    <Link :href="route('inertia.index')">名前付きルートの確認です</Link>
+    <Link :href="route('inertia.index')">名前付きルートの確認です</Link><br>
+    <Link :href="route('inertia.show', { id : 50 })">ルートパラメータのテスト</Link>
 
 </template>
 <!-- .wslconfigのテスト -->
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,8 @@ Route::get('/inertia-test', function () {
 
 Route::get('/inertia/index', [InertiaTestController::class, 'index'])->name('inertia.index');
 
+Route::get('/inertia/show/{id}', [InertiaTestController::class, 'show'])->name('inertia.show');
+
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
