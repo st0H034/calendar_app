cd709c5e69a03f8efbcff3bb3a46dc7b72623737
st0H034 <kd1307152@st.kobedenshi.ac.jp>
2023-06-13 21:30:12 +0900

    update: 使わないルートをコメントアウト

--- routes/web.php
+++ routes/web.php
@@ -17,19 +17,21 @@ use App\Http\Controllers\ScheduleController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+/*
 Route::get('/inertia-test', function () {
     return Inertia::render('InertiaTest');
 });
+*/
 
 Route::get('/calendar', function () {
     return Inertia::render('Calendar');
 })->name('calendar');
 
-Route::get('/inertia/index', [InertiaTestController::class, 'index'])->name('inertia.index');
+//Route::get('/inertia/index', [InertiaTestController::class, 'index'])->name('inertia.index');
 
-Route::get('/inertia/show/{id}', [InertiaTestController::class, 'show'])->name('inertia.show');
+//Route::get('/inertia/show/{id}', [InertiaTestController::class, 'show'])->name('inertia.show');
 
-Route::post('/inertia', [InertiaTestController::class, 'store'])->name('inertia.store');
+//Route::post('/inertia', [InertiaTestController::class, 'store'])->name('inertia.store');
 
 Route::post('/Calendar/store', [ScheduleController::class, 'store'])->name('calendar.store');
 
@@ -37,6 +39,7 @@ Route::post('/Calendar/get', [ScheduleController::class, 'get'])->name('calendar
 
 Route::post('/Calendar/delete', [ScheduleController::class, 'delete'])->name('calendar.delete');
 
+/*
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
@@ -45,15 +48,20 @@ Route::get('/', function () {
         'phpVersion' => PHP_VERSION,
     ]);
 });
+*/
 
+/*
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+*/
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
 
 require __DIR__.'/auth.php';
